8263919b945b06fbdbe57d40047e1aea4f8621f1
Javier Galicia <jgaliciac6@miumg.edu.gt>
2026-08-13 19:12:38 -0600

    feat: clase usuario

---
+++ backend/ExamenNeonetApi/ExamenNeonetApi/Model/Usuario.cs
@@ -0,0 +1,11 @@
+﻿namespace ExamenNeonetApi.Model
+{
+	public class Usuario
+	{
+		public int USRId { get; set; }
+		public string USRNombre { get; set;}
+		public string USRFechaCreacion { get; set;}
+		public bool USREstado { get; set;}
+		public string USRPassword { get; set;}
+	}
+}
