c0ad3e55f6afc3f5cb7f481010c1bb190fa5b7e4
Javier Galicia <jgaliciac6@miumg.edu.gt>
2026-08-13 19:08:27 -0600

    feat: configuracion conexion base de datos

---
+++ backend/ExamenNeonetApi/ExamenNeonetApi/Datos/Conexion.cs
@@ -0,0 +1,19 @@
+﻿namespace RegistroSucursales.Datos
+{
+    public class Conexion
+    {
+        public string? cadenaSQL = string.Empty;
+
+        public Conexion()
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+
+            cadenaSQL = builder.GetSection("ConnectionStrings:CadenaSQL").Value;
+        }
+
+        public string? GetCadenaSQL()
+        {
+            return cadenaSQL;
+        }
+    }
+}
